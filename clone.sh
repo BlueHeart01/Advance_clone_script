690a371dae380ce8355e5ff7e7f4800f83ff3d83
Sayan016 <98513272+BlueHeart01@users.noreply.github.com>
2026-05-07 01:33:34 +0530

    Update clone script for new repository paths

--- clone.sh
+++ clone.sh
@@ -74,8 +74,8 @@ DEVICE_REPO="https://github.com/BlueHeart01/device_xiaomi_redwood.git"
 clone_repo_branch "$DEVICE_REPO"
 
 # ─── Clone Trees ──────────────────────────────────────────
-clone_repo_with_branch "$DEVICE_REPO" device/xiaomi/redwood "$SELECTED_BRANCH"
-clone_repo https://github.com/BlueHeart01/vendor_xiaomi_redwood.git vendor/xiaomi/redwood
+clone_repo_with_branch "$DEVICE_REPO" android_device/xiaomi/redwood "$SELECTED_BRANCH"
+clone_repo https://github.com/BlueHeart01/android_vendor_xiaomi_redwood.git vendor/xiaomi/redwood
 clone_repo https://github.com/Redwood-AOSP/android_device_xiaomi_redwood-kernel.git device/xiaomi/redwood-kernel
 clone_repo https://github.com/BlueHeart01/redwood_vendor_xiaomi_redwood-miuicamera.git vendor/xiaomi/redwood-miuicamera
 clone_repo https://github.com/BlueHeart01/hardware_dolby.git hardware/dolby
